94a0452e0b31ce28ddd2dac7505bce9593bfa261
manuyeldev <manuyeldev@yelsfstudio.com>
2023-10-06 14:14:35 -0500

    59. Player Movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var jump_force = 250
+@export var speed = 125
+
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("move_right"):
-		animated_sprite.play("run")
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		if (velocity.y > 500):
+			velocity.y = 500
+		
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force	
+		
+	var direction = Input.get_axis("move_left", "move_right")
+	print(direction)
+	velocity.x = direction * speed
+		
+	move_and_slide()
+	
